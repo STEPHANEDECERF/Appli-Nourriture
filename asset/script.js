4175c87664252ff41fa8dd997a32bcc3ba3340ab
stephanedecerf <decerfstephane1972@gmail.com>
2025-01-22 16:22:55 +0100

    nutriscore ok

--- asset/script.js
+++ asset/script.js
@@ -4,29 +4,33 @@ id = urlParams.get(`id`)
 
 
 fetch(`https://world.openfoodfacts.org/api/v3/product/${id}`)
-.then(response => response.json())
-.then(data => {
-console.log(data)
-console.log(data.product.id)
-console.log(data.product.categories)
-console.log(data.product.ingredients_text_fr)
-console.log(data.product.product_name_fr)
-console.log(data.product.quantity)
-console.log(data.product.ecoscore_grade)
-console.log(data.product.ecoscore_score)
-console.log(data.product.nutriscore_grade)
-console.log(data.product.nova_groups)
-console.log(data.product.brands)
-console.log(data.product)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log(data.product.id)
+        console.log(data.product.categories)
+        console.log(data.product.ingredients_text_fr)
+        console.log(data.product.product_name_fr)
+        console.log(data.product.quantity)
+        console.log(data.product.ecoscore_grade)
+        console.log(data.product.ecoscore_score)
+        console.log(data.product.nutriscore_grade)
+        console.log(data.product.nova_groups)
+        console.log(data.product.brands)
+        console.log(data.product)
 
-if (data.product.nova_groups!==undefined && data.product.nova_groups!==null){
-    nova = `<img src="./asset/img/Novascore/Nova${data.product.nova_groups}.svg" alt="" class=''>`
-}
-if (data.product.nutriscore_grade!==undefined && data.product.nutriscore_grade!==null){
-    nutriscore = `<img src="./asset/img/Nutriscore/${data.product.nutriscore_grade}.svg" alt="" class=''>`
-}
+        if (data.product.nova_groups !== undefined || data.product.nova_groups !== null) {
+            nova = `<img src="./asset/img/Novascore/Nova${data.product.nova_groups}.svg" alt="" class=''>`
+            console.log(nova)
+        }
 
-document.getElementById("interface").innerHTML = `
+        console.log(data.product.nutriscore_grade)
+        if (data.product.nutriscore_grade !== undefined || data.product.nutriscore_grade !== null) {
+            nutriscore = `<img src="./asset/img/Nutriscore/${data.product.nutriscore_grade}.svg" alt="" class=''>`
+            console.log(nutriscore)
+        }
+
+        document.getElementById("interface").innerHTML = `
    <div class="text-white">
             <p class="fs-3 mt-2 text-center fw-bold">${data.product.product_name_fr}</p>
             <div class="text-center">
@@ -47,4 +51,4 @@ document.getElementById("interface").innerHTML = `
         </div>`
 
 
-})
+    })
